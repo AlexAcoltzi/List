3e3b5f49d8efcfa6bcbffae839bdfe7107534e62
AlexAcoltzi <alex.fantastico22@gmail.com>
2020-10-19 18:57:38 -0500

    creating a simple linked list

--- Simple-list/Node.h
+++ Simple-list/Node.h
@@ -1,12 +1,15 @@
 #ifndef NODE_H
 #define NODE_H
 #include "Student.h"
+#include "Simple-List.h"
 
 #endif // NODE_H
 class Node
 {
 private:
- alumno Alumno;
+ Student student;
+ Node* next;
 public:
-    Node() {}
+    Node(Student);
+    friend class Simple_List;
 };
